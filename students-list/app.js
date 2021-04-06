af311f760f5d849017caa498596e427b0f035030
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-04-06 16:03:37 +1100

    first commit

--- students-list/app.js
+++ students-list/app.js
@@ -1,1 +1,10 @@
-console.log('students', students);
+for (i in students) {
+    var newLi = document.createElement("li"); //variable pour créer des élements li 
+    var items = document.getElementById('items');
+    var a = Object.entries(students[i]);
+    var test = document.createTextNode(a);
+    newLi.appendChild(test);
+    document.getElementById("items").appendChild(newLi);
+
+}
+// console.log('students', students);

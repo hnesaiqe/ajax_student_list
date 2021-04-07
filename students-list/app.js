b4ded91f56c09909389c334f29b89be5fc2db234
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-04-07 15:40:48 +1100

    first commit

--- students-list/app.js
+++ students-list/app.js
@@ -1,10 +1,36 @@
-for (i in students) {
-    var newLi = document.createElement("li"); //variable pour créer des élements li 
-    var items = document.getElementById('items');
-    var a = Object.entries(students[i]);
-    var test = document.createTextNode(a);
-    newLi.appendChild(test);
-    document.getElementById("items").appendChild(newLi);
+// for (i in students) {
+// var liNew = document.createElement("table");  
+// var items = document.getElementById('items');
+// var x = (students[i]);
+// var creatLi = document.createTextNode(x.id + ' ' + x.name + ' ' + x.createdAt + ' ' + x.avatar);
+// liNew.appendChild(creatLi);
+//     document.getElementById("items").appendChild(liNew);
 
+// }
+
+var table = document.getElementById("items");
+
+function displayContent(content) {
+    content.forEach(function (data) {
+
+        table.innerHTML +=
+
+            '' + data.id +
+            " " + data.name +
+            " " + data.createdAt +
+            " " + data.avatar +
+            " " + '<button type="button" class="btn btn-outline-danger btn-sm m-1">Delete</button>' +
+            " " + '<button type="button" class="btn btn-outline-info btn-sm m-1">Edit</button>' +
+            
+            '<br>'
+    });
 }
-// console.log('students', students);
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        displayContent(JSON.parse(this.responseText));
+    }
+
+};
+xhttp.open("GET", "students.json", true);
+xhttp.send();

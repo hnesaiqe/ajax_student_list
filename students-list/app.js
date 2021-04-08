2cb9bc62bbeb6260b1e656065eee627b84a6b661
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-04-08 16:06:49 +1100

    Ajax Delete

--- students-list/app.js
+++ students-list/app.js
@@ -1,5 +1,5 @@
 // for (i in students) {
-// var liNew = document.createElement("table");  
+// var liNew = document.createElement("table");
 // var items = document.getElementById('items');
 // var x = (students[i]);
 // var creatLi = document.createTextNode(x.id + ' ' + x.name + ' ' + x.createdAt + ' ' + x.avatar);
@@ -8,20 +8,24 @@
 
 // }
 
+// Requete GET
+
 var table = document.getElementById("items");
 
 function displayContent(content) {
     content.forEach(function (data) {
 
         table.innerHTML +=
+            '<li class="newLi col-sm-6 col-lg-3" style="list-style : none;" id="newLi_' + data.id + '">' +
+            data.id +
+            "<br>" + '<img class= "h-25 w-auto" src="./avatar.png" alt="">' +
 
-            '' + data.id +
-            " " + data.name +
-            " " + data.createdAt +
-            " " + data.avatar +
-            " " + '<button type="button" class="btn btn-outline-danger btn-sm m-1">Delete</button>' +
-            " " + '<button type="button" class="btn btn-outline-info btn-sm m-1">Edit</button>' +
-            
+            "<br>" + data.name +
+            "<br>" + data.createdAt +
+
+            "<br>" + '<button type="button" class="deleteBtn btn btn-outline-danger btn-sm m-1" id="deleted" onclick="deleteList()">Delete</button>' +
+            " " + '<button type="button" class="editBtn btn btn-outline-info btn-sm m-1" id="edited">Edit</button>' +
+            '</li>' +
             '<br>'
     });
 }
@@ -34,3 +38,18 @@ xhttp.onreadystatechange = function () {
 };
 xhttp.open("GET", "students.json", true);
 xhttp.send();
+
+// Delete
+
+function deleteList() {
+
+    document.addEventListener("click", myFunction);
+    // console.log(document.querySelectorAll(".deleteBtn"));
+
+    // document.querySelectorAll(".deleteBtn").addEventListener("click", myFunction);
+    function myFunction(e) {
+        e.target.parentNode.remove();
+        console.log("e.target.parentNode");
+        
+    }
+}
